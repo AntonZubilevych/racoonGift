d6c6630820aeb45a7fe971b4a7bc715e97091e8c
antonzubb <antonzubb@gmail.com>
2018-08-02 14:29:01 +0300

    add Statistics feathers

---
+++ src/Controller/StatisticsController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anton
+ * Date: 02.08.18
+ * Time: 13:49
+ */
+
+namespace App\Controller;
+
+
+class StatisticsController
+{
+
+}
---
+++ src/GiftFinderTool/GiftStatistics.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anton
+ * Date: 02.08.18
+ * Time: 14:01
+ */
+
+namespace App\GiftFinderTool;
+
+
+class GiftStatistick
+{
+
+}
